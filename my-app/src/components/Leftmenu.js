4067730621420447ec18ac66735a8ecc5c6886aa
MayThetKhine <usagishiro.1996@gmail.com>
2019-06-10 17:55:32 -0700

    React single page Movie app

---
+++ my-app/src/components/Leftmenu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { FaStar,FaAlignJustify } from 'react-icons/lib/fa';
+
+const element = (
+
+<div className="leftmenu">
+  <Nav.Item>
+    <Nav.Link className="mt-1" eventKey="home" href="/"><FaAlignJustify /></Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="detail" href="/detail"><FaStar /></Nav.Link>
+  </Nav.Item>
+</div>
+
+);
+
+export default class Leftmenu extends React.Component {
+  render() {return (element);}
+}
